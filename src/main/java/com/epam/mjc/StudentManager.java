68adf6a01032b801ff08b0b2c6e1752d6b25dd85
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-29 11:55:09 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,21 +5,28 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    Student result = Student.getValueOf(studentID);
-    if(result == null){
-      throw new StudentNotFoundException("Could not find student with ID " + studentID);        
-    }
-    return result;
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+public Student find(long studentID) {
+        Student result = Student.getValueOf(studentID);
+        if(result == null){
+            throw new StudentNotFoundException("Could not find student with ID " + studentID);
+        }
+        return result;
     }
 
-  }
+    public static void main(String[] args) throws StudentNotFoundException {
+        StudentManager manager = new StudentManager();
+
+
+        for (int i = 0; i < IDs.length; i++) {
+            try {
+                Student student = manager.find(IDs[i]);
+                System.out.println("Student name " + student.getName());
+            } catch (NullPointerException e) {
+                
+            }
+
+        }
+    }
+
+
 }
